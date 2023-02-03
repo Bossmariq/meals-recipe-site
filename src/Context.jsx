38591fb9b1b23234c809d87301140f96932c3fe5
sire67 <uchepibe@gmail.com>
2023-02-03 10:50:03 +0100

    fix/added like button func

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,115 +1,103 @@
-import React, { useContext, useEffect, useState } from "react";
-import axios from "axios";
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 
 const AppContext = React.createContext();
 
-const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
-const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 const getFavoritesFromLocalStorage = () => {
-  let favorites = localStorage.getItem("favorites");
+  let favorites = localStorage.getItem('favorites');
   if (favorites) {
-    favorites = JSON.parse(localStorage.getItem("favorites"));
+    favorites = JSON.parse(localStorage.getItem('favorites'));
   } else {
-    favorites = [];
+    favorites = []
   }
   return favorites;
-};
+}
 
 const AppProvider = ({ children }) => {
+
   const [meals, setMeals] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
   const [showModal, setShowModal] = useState(false);
   const [selectedMeal, setSelectedMeal] = useState(null);
-  const [favorites, setFavorites] = useState(getFavoritesFromLocalStorage());
-  const [liked, setLiked] = useState(false);
+  const [favorites, setFavorites] = useState(getFavoritesFromLocalStorage()); 
+  const [liked, setLiked] = useState(undefined);
 
   const fetchMeals = async (url) => {
     setLoading(true);
     try {
       // const response = await axios(url);
-      const { data } = await axios(url);
+      const { data } = await axios(url)
       if (data.meals) {
-        setMeals(data.meals);
+        setMeals(data.meals)
       } else {
-        setMeals([]);
+        setMeals([])
       }
+
     } catch (error) {
-      console.log(error.response);
+      console.log(error.response)
     }
     setLoading(false);
-  };
+  }
 
   const fetchRandomMeal = () => {
-    fetchMeals(randomMealUrl);
-  };
+    fetchMeals(randomMealUrl)
+  }
 
   const selectMeal = (idMeal, favoriteMeal) => {
     let meal;
     if (favoriteMeal) {
       meal = favorites.find((meal) => meal.idMeal === idMeal);
+
     } else {
       meal = meals.find((meal) => meal.idMeal === idMeal);
     }
 
     setSelectedMeal(meal);
     setShowModal(true);
-  };
+  }
 
   const closeModal = () => {
-    setShowModal(false);
-  };
+    setShowModal(false)
+  }
 
   const addToFavorites = (idMeal) => {
     const alreadyFavorite = favorites.find((meal) => meal.idMeal === idMeal);
-    if (alreadyFavorite) return;
+    if (alreadyFavorite) return
     const meal = meals.find((meal) => meal.idMeal === idMeal);
     const updatedFavorites = [...favorites, meal];
     setFavorites(updatedFavorites);
-    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
-  };
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+  }
 
   const removeFromFavorites = (idMeal) => {
     const updateFavorites = favorites.filter((meal) => meal.idMeal !== idMeal);
     setFavorites(updateFavorites);
-    localStorage.setItem("favorites", JSON.stringify(updateFavorites));
-  };
+    localStorage.setItem('favorites', JSON.stringify(updateFavorites));
+  }
 
   useEffect(() => {
     fetchMeals(allMealsUrl);
-  }, []);
+  }, [])
 
   useEffect(() => {
-    if (!searchTerm) return;
+    if (!searchTerm) return
     fetchMeals(`${allMealsUrl}${searchTerm}`);
-  }, [searchTerm]);
+  }, [searchTerm])
+
 
   return (
-    <AppContext.Provider
-      value={{
-        meals,
-        loading,
-        setSearchTerm,
-        fetchRandomMeal,
-        showModal,
-        selectMeal,
-        selectedMeal,
-        closeModal,
-        favorites,
-        addToFavorites,
-        removeFromFavorites,
-        liked,
-        setLiked,
-      }}
-    >
+    <AppContext.Provider value={{ meals, loading, setSearchTerm, fetchRandomMeal, showModal, selectMeal, selectedMeal, closeModal, favorites, addToFavorites, removeFromFavorites, liked, setLiked }}>
       {children}
     </AppContext.Provider>
-  );
-};
+  )
+}
 
 export const useGlobalContext = () => {
-  return useContext(AppContext);
-};
+  return useContext(AppContext)
+}
 
 export { AppContext, AppProvider };
